64f32fd2056e09b0aff3848d8c749e072eb99518
Armando233-abc <armandovigliotti5@gmail.com>
2021-04-15 13:04:11 +0200

    with class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(){
+    super()
+
+    this.state={
+      expression : ''
+    }
+  }
+
+  render(){
+    return(
+      <div id='main'>
+        <form id='number'>
+          <Button variant="contained" color="primary">0</Button>
+          <Button variant="contained" color="primary">1</Button>
+          <Button variant="contained" color="primary">2</Button>
+          <Button variant="contained" color="primary">3</Button>
+          <Button variant="contained" color="primary">4</Button>
+          <Button variant="contained" color="primary">5</Button>
+          <Button variant="contained" color="primary">6</Button>
+          <Button variant="contained" color="primary">7</Button>
+          <Button variant="contained" color="primary">8</Button>
+          <Button variant="contained" color="primary">9</Button>
+        </form>
+
+        <form id='operazioni'>
+          <Button variant="contained" color="primary">+</Button>
+          <Button variant="contained" color="primary">-</Button>
+          <Button variant="contained" color="primary">*</Button>
+          <Button variant="contained" color="primary">/</Button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
